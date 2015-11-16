7a74ae8183b84d77cf969843a29af587df8cacdd
Kirill <ay@mail.by>
2015-11-16 22:23:37 +0300

    Testing controller methods

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -1,6 +1,40 @@
 require 'rails_helper'
 
 describe ContactsController do
+  describe "GET #index" do
+    context "with params[:letter]" do
+      it "populates an array of contacts starting with the letter" do
+        smith = create(:contact, lastname: "Smith")
+        jones = create(:contact, lastname: "Jones")
+        get :index, letter: "S"
+        
+        expect(assigns(:contacts)).to match_array([smith])
+      end
+      
+      it "renders the :index template" do
+        get :index, letter: "S"
+        
+        expect(response).to render_template :index
+      end
+    end
+    
+    context "without params[:letter]" do
+      it "populates an array of all contacts" do
+        smith = create(:contact, lastname: "Smith")
+        jones = create(:contact, lastname: "Jones")
+        get :index
+        
+        expect(assigns(:contacts)).to match_array([smith, jones])        
+      end
+      
+      it "renders the :index template" do
+        get :index
+
+        expect(response).to render_template :index
+      end
+    end
+  end
+  
   describe "GET #show" do
     it "assigns the requested contact to @contact" do
       contact = create(:contact)
@@ -14,6 +48,58 @@ describe ContactsController do
       get :show, id: contact
       
       expect(response).to render_template :show
+    end
+  end
+  
+  describe "GET #new" do
+    it "assigns a new Contact to @contact" do
+      get :new
+      
+      expect(assigns(:contact)).to be_a_new(Contact)
+    end
+    
+    it "renders the :new template" do
+      get :new
+      
+      expect(response).to render_template :new
+    end
+  end
+  
+  describe "GET #edit" do
+    it "assigns the requested contact to @contact" do
+      contact = create(:contact)
+      get :edit, id: contact
+      
+      expect(assigns(:contact)).to eq contact
+    end
+    
+    it "renders the :edit template" do
+      contact = create(:contact)
+      get :edit, id: contact
+      
+      expect(response).to render_template :edit
+    end
+  end
+  
+  describe "POST #create" do
+    before :each do
+      @phones = [
+          attributes_for(:phone),
+          attributes_for(:phone),
+          attributes_for(:phone)
+      ]
+    end
+    
+    context "with valid attributes" do
+      it "saves the new Contact in the database" do
+        expect{ post :create, contact: attributes_for(:contact) }.to change(Contact, :count).by(1)
+      end
+      
+      it "redirects to contacts#show" do
+        post :create, contact: attributes_for(:contact)
+        
+        expect(response).to redirect_to contact_path(assigns[:contact])
+      end
     end
   end
 end
